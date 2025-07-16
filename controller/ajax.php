82223c8e07140c7bc520278fce544993c12a3dc5
Sylver35 <33929219+Sylver35@users.noreply.github.com>
2025-07-16 12:07:40 +0200

    new version 1.5.1

--- controller/ajax.php
+++ controller/ajax.php
@@ -39,6 +39,7 @@ class ajax
 	 */
 	public function construct_ajax($action, $forum_id = 0)
 	{
+		$this->language->add_lang('posting');
 		$message = $this->request->variable('content', '', true);
 		$uid = $bitfield = $options = $subject = '';
 		$update_count = [];
@@ -49,29 +50,35 @@ class ajax
 		// In message form, subject and attachements job
 		if ($action === 'message')
 		{
-			$this->language->add_lang('posting');
 			$subject = $this->request->variable('subject', '', true);
-			$total = $this->request->variable('total', 0);
-			$attachements = $plupload = [];
-			for ($i = 0; $i < $total; $i++)
+			$attach = $this->request->variable('attach', 0);
+
+			if ($attach)
 			{
-				$plupload[$i] = $this->request->variable('plupload-' . $i, '', true);
-				list($attach_id, $is_orphan, $attach_comment, $real_filename, $filesize) = explode(',', $plupload[$i]);
-				$attachements[$i] = [
-					'attach_id'			=> (int) $attach_id,
-					'is_orphan'			=> (int) $is_orphan,
-					'attach_comment'	=> (string) $attach_comment,
-					'real_filename'		=> (string) $real_filename,
-					'filesize'			=> (int) $filesize,
-				];
+				$attachements = $plupload = [];
+				$total = $this->request->variable('total', 0);
+				for ($i = 0; $i < $total; $i++)
+				{
+					$plupload[$i] = $this->request->variable('plupload-' . $i, '', true);
+					list($attach_id, $is_orphan, $attach_comment, $real_filename, $filesize) = explode(',', $plupload[$i]);
+					$attachements[$i] = [
+						'attach_id'			=> (int) $attach_id,
+						'is_orphan'			=> (int) $is_orphan,
+						'attach_comment'	=> (string) $attach_comment,
+						'real_filename'		=> (string) $real_filename,
+						'filesize'			=> (int) $filesize,
+					];
+				}
+				parse_attachments($forum_id, $message, $attachements, $update_count, true);
 			}
-			parse_attachments($forum_id, $message, $attachements, $update_count, true);
+
 			generate_text_for_storage($subject, $uid, $bitfield, $options, true, false, true);
 			$subject = generate_text_for_display($subject, $uid, $bitfield, 1 | 2, false);
 		}
 
 		$message = $this->replace_text($message);
 
+		// Send the preview to the browser now
 		$json_response = new \phpbb\json_response;
 		$json_response->send([
 			'content'		=> $message,
@@ -87,7 +94,7 @@ class ajax
 	 */
 	private function replace_text($message)
 	{
-		// Always display images/smilies with correct url
+		// Always display images and smilies with correct url
 		$message = str_replace(['src="./../../', 'src="./../', 'src="./'], 'src="' . generate_board_url() . '/', $message);
 
 		// A litle magic for simple mention ext
